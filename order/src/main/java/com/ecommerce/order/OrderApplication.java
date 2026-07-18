4cb06c13eb148589387b332096da6a76b310a3e7
ketantaluja5 <ketantaluja5@gmail.com>
2026-07-18 18:10:11 +0530

    Added Order Service

--- order/src/main/java/com/ecommerce/order/OrderApplication.java
+++ order/src/main/java/com/ecommerce/order/OrderApplication.java
@@ -1,4 +1,4 @@
-package com.ecommerce.product;
+package com.ecommerce.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,11 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.TimeZone;
 
 @SpringBootApplication
-public class ProductApplication {
+public class OrderApplication {
 
 	public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
-        SpringApplication.run(ProductApplication.class, args);
+        SpringApplication.run(OrderApplication.class, args);
 	}
 
 }
